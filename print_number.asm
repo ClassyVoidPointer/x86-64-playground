554e9b187dc67e80a8e92f54a58331199cea3f76
Alex D'agostino <alexdagostino@MacBook-Air-di-Alex.local>
2023-11-23 00:41:12 +0100

    printing a number

---
+++ print_number.asm
@@ -0,0 +1,21 @@
+; this is a program that places a number into a register and prints it
+
+global _start:
+number: 	db 	9
+
+_start:
+    mov r8, 48 			; first store the position of '0' in the ascii table
+    mov r9, number 		; move into r9 the number
+    add r8, r9 			; move the result of the addition into the r8 register '9'
+
+    mov rax, 1 			; write syscall
+    mov rdi, 1			; stdout file handle
+    mov rsi, r8 		
+    mov rdx, 1
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
+
+
