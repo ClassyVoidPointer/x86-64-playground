4bff02e04fe293f83b4951801483e33eddee2ab5
Alex D'agostino <alexdagostino@MacBook-Air-di-Alex.local>
2023-11-23 00:43:21 +0100

    print_numbers

--- print_number.asm
+++ print_number.asm
@@ -5,7 +5,7 @@ number: 	db 	9
 
 _start:
     mov r8, 48 			; first store the position of '0' in the ascii table
-    mov r9, number 		; move into r9 the number
+    mov r9, [number] 		; move into r9 the number
     add r8, r9 			; move the result of the addition into the r8 register '9'
 
     mov rax, 1 			; write syscall
